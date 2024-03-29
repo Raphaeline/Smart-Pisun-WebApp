6185c88a1088b8975f8bed3132b0cd0f25f68702
Raphaeline <zlfkhr.akbar@gmail.com>
2024-03-29 12:49:43 +0700

    fixing bug

--- src/firebase/middleware/requireAuth.ts
+++ src/firebase/middleware/requireAuth.ts
@@ -3,16 +3,17 @@ import { getAuth } from "firebase/auth";
 
 const auth = getAuth(); // Initialize your Firebase auth instance here
 
-const requireAuth = (handler) => async (req: NextApiRequest, res: NextApiResponse) => {
+const requireAuth = (handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>) => async (req: NextApiRequest, res: NextApiResponse) => {
   // Check if user is authenticated
   const user = auth.currentUser;
   if (!user) {
     // If user is not authenticated, return unauthorized response
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
   // If user is authenticated, proceed to the main handler
   return handler(req, res);
 };
+
 
 export default requireAuth;
