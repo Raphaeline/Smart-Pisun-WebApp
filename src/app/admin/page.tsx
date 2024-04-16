9ee9f88f610697cf20d7252bb9bbf14c44049687
Raphaeline <zlfkhr.akbar@gmail.com>
2024-04-16 14:56:05 +0700

    add admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,25 +1,59 @@
-'use client'
-import { useAuthContext } from "@/context/AuthContext";
+"use client";
+import signUp from "@/firebase/auth/signup";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useState } from "react";
+import Navbar from "../../../components/Navbar";
 
 function Page(): JSX.Element {
-  // Access the user object from the authentication context
-  // const { user } = useAuthContext();
-  const { user } = useAuthContext() as { user: any }; // Use 'as' to assert the type as { user: any }
-  const router = useRouter();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const router = useRouter();
 
-  useEffect( () => {
-    // Redirect to the home page if the user is not logged in
-    if ( user == null ) {
-      router.push( "/" );
-    }
-    // }, [ user ] );
-  }, [ user, router ] ); // Include 'router' in the dependency array to resolve eslint warning
+	// Handle form submission
+	const handleForm = async (event: { preventDefault: () => void }) => {
+		event.preventDefault();
 
-  return (
-    <h1>Only logged-in users can view this page</h1>
-  );
+		// Attempt to sign up with provided email and password
+		const { result, error } = await signUp(email, password);
+
+		if (error) {
+			// Display and log any sign-up errors
+			console.log(error);
+			return;
+		}
+
+		// Sign up successful
+		console.log(result);
+
+		// Redirect to the admin page
+		router.push("/dashboard");
+	};
+
+	return (
+		<div className="flex justify-center items-center h-screen text-black bg-[#f4f9fb]">
+			<Navbar isDashboardPage={false} isProdukPage={false} isAdminPage={true} />
+			<div className="w-96 bg-white rounded shadow p-6">
+				<h1 className="text-3xl font-bold mb-6 font-jakarta">Registration</h1>
+				<form onSubmit={handleForm} className="space-y-4">
+					<div>
+						<label htmlFor="email" className="block mb-1 font-medium font-jakarta">
+							Email
+						</label>
+						<input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" className="w-full border border-gray-300 rounded px-3 py-2 font-jakarta" />
+					</div>
+					<div>
+						<label htmlFor="password" className="block mb-1 font-medium font-jakarta">
+							Password
+						</label>
+						<input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" className="w-full border border-gray-300 rounded px-3 py-2 font-jakarta" />
+					</div>
+					<button type="submit" className="w-full bg-[#0D6D8C] text-white font-jakarta font-semibold py-2 rounded">
+						Sign up
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Page;
--- src/app/produk/page.tsx
+++ src/app/produk/page.tsx
@@ -7,192 +7,203 @@ import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 // import requireAuth from "../../firebase/middleware/requireAuth";
 
 const Navbar = ({ isDashboardPage, isProdukPage, isAdminPage }: { isDashboardPage: boolean; isProdukPage: boolean; isAdminPage: boolean }) => {
-  return (
-    <nav className="w-[200px] xl:w-[250px] fixed bg-white top-0 left-0 right-0 min-h-full shadow-xl">
-      <div className="flex p-[20px]">
-        <div className="m-[10px] w-[50px]">
-          <img src="/Logo1.svg" alt="My Image" />
-        </div>
-        <div className="m-[6px] 2xl:m-[10px] flex flex-col">
-          <p className="text-[#0D6D8C] text-[14px] 2xl:text-[18px] font-jakarta font-semibold tracking-[5.6px]">TuBi</p>
-          <p className="text-[#0D6D8C] text-[16px] 2xl:text-[20px] font-jakarta font-semibold whitespace-nowrap overflow-hidden">Manajer Toko</p>
-        </div>
-      </div>
-      <ul>
-        <li>
-          <a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="/dashboard">
-            <img src={isDashboardPage ? "/dashboard_blue.svg" : "/dashboard.svg"} alt="Dashboard" className="w-[18px] 2xl:w-[28px]" />
-            <p className={isDashboardPage ? "text-[#25B6D0] font-poppins text-[16px] 2xl:text-[20px] px-[6px]" : "text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]"}>Dasboard</p>
-          </a>
-        </li>
-        <li>
-          <a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="/produk">
-            <img src={isProdukPage ? "/produk_blue.svg" : "/produk.svg"} alt="Produk" className="w-[18px] 2xl:w-[28px]" />
-            <p className={isProdukPage ? "text-[#25B6D0] font-poppins text-[16px] 2xl:text-[20px] px-[6px]" : "text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]"}>Produk</p>
-          </a>
-        </li>
-        <li>
-          <a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="#">
-            <img src={isAdminPage ? "/admin_blue.svg" : "/admin.svg"} className="w-[18px] 2xl:w-[28px]" />
-            <p className="text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]">Admin</p>
-          </a>
-        </li>
-        <li>
-          <a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="#">
-            <img src="/bantuan.svg" alt="Dashboard" className="w-[18px] 2xl:w-[28px]" />
-            <p className="text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]">Bantuan</p>
-          </a>
-        </li>
-        <div className="flex px-[28px] py-[14px] 2xl:py-[20px]">
-          <button
-            className=" bg-[#FF5C5C] hover:bg-[#FF5C5C] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md hover:shadow-lg"
-            onClick={() => (window.location.href = "/")}
-          >
-            Logout
-          </button>
-        </div>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className="w-[200px] xl:w-[250px] fixed bg-white top-0 left-0 right-0 min-h-full shadow-xl">
+			<div className="flex p-[20px]">
+				<div className="m-[10px] w-[50px]">
+					<img src="/Logo1.svg" alt="My Image" />
+				</div>
+				<div className="m-[6px] 2xl:m-[10px] flex flex-col">
+					<p className="text-[#0D6D8C] text-[14px] 2xl:text-[18px] font-jakarta font-semibold tracking-[5.6px]">TuBi</p>
+					<p className="text-[#0D6D8C] text-[16px] 2xl:text-[20px] font-jakarta font-semibold whitespace-nowrap overflow-hidden">Manajer Toko</p>
+				</div>
+			</div>
+			<ul>
+				<li>
+					<a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="/dashboard">
+						<img src={isDashboardPage ? "/dashboard_blue.svg" : "/dashboard.svg"} alt="Dashboard" className="w-[18px] 2xl:w-[28px]" />
+						<p className={isDashboardPage ? "text-[#25B6D0] font-poppins text-[16px] 2xl:text-[20px] px-[6px]" : "text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]"}>Dasboard</p>
+					</a>
+				</li>
+				<li>
+					<a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="/produk">
+						<img src={isProdukPage ? "/produk_blue.svg" : "/produk.svg"} alt="Produk" className="w-[18px] 2xl:w-[28px]" />
+						<p className={isProdukPage ? "text-[#25B6D0] font-poppins text-[16px] 2xl:text-[20px] px-[6px]" : "text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]"}>Produk</p>
+					</a>
+				</li>
+				<li>
+					<a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="/admin">
+						<img src={isAdminPage ? "/admin_blue.svg" : "/admin.svg"} className="w-[18px] 2xl:w-[28px]" />
+						<p className={isAdminPage ? "text-[#25B6D0] font-poppins text-[16px] 2xl:text-[20px] px-[6px]" : "text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]"}>Admin</p>
+					</a>
+				</li>
+				<li>
+					<a className="flex px-[28px] py-[14px] 2xl:py-[20px]" href="#">
+						<img src="/bantuan.svg" alt="Dashboard" className="w-[18px] 2xl:w-[28px]" />
+						<p className="text-[#000000] font-poppins text-[16px] 2xl:text-[20px] px-[6px]">Bantuan</p>
+					</a>
+				</li>
+				<div className="flex px-[28px] py-[14px] 2xl:py-[20px]">
+					<a className=" bg-[#FF5C5C] hover:bg-[#FF5C5C] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md hover:shadow-lg" href="/">
+						Logout
+					</a>
+				</div>
+			</ul>
+		</nav>
+	);
 };
 
 interface ProdukData {
-  id: string;
-  Nama: string;
-  Harga: number;
+	id: string;
+	Nama: string;
+	Harga: number;
 }
 
 async function fetchDataFromFirestore(): Promise<ProdukData[]> {
-  const querySnapshots = await getDocs(collection(db, "produk"));
-  const data: ProdukData[] = [];
-  querySnapshots.forEach((doc) => {
-    data.push({ id: doc.id, ...doc.data() } as ProdukData);
-  });
-  return data;
+	const querySnapshots = await getDocs(collection(db, "produk"));
+	const data: ProdukData[] = [];
+	querySnapshots.forEach((doc) => {
+		data.push({ id: doc.id, ...doc.data() } as ProdukData);
+	});
+	return data;
 }
 
 const Produk = () => {
-  const [produkData, setProdukData] = useState<ProdukData[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [filter, setFilter] = useState<string>("produk");
-  const itemsPerPage: number = 5;
-  const router = useRouter();
+	const [produkData, setProdukData] = useState<ProdukData[]>([]);
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const [filter, setFilter] = useState<string>("produk");
+	const itemsPerPage: number = 5;
+	const router = useRouter();
 
-  // const handleEdit = (id: string, Nama: string, Harga: number) => {
-  //   console.log("Tombol edit ditekan untuk id:", id);
-  //   router.push(`/produk/edit/${id}?Nama=${Nama}&Harga=${Harga}`);
-  // };
+	// const handleEdit = (id: string, Nama: string, Harga: number) => {
+	//   console.log("Tombol edit ditekan untuk id:", id);
+	//   router.push(`/produk/edit/${id}?Nama=${Nama}&Harga=${Harga}`);
+	// };
 
-  const handleDelete = async (id: string) => {
-    const isConfirmed = window.confirm("Apakah Anda yakin ingin menghapus produk ini?");
-    if (isConfirmed) {
-      await deleteDoc(doc(db, "produk", id));
-      setProdukData((prevData) => prevData.filter((item) => item.id !== id));
-    }
-  };
+	const handleDelete = async (id: string) => {
+		const isConfirmed = window.confirm("Apakah Anda yakin ingin menghapus produk ini?");
+		if (isConfirmed) {
+			await deleteDoc(doc(db, "produk", id));
+			setProdukData((prevData) => prevData.filter((item) => item.id !== id));
+		}
+	};
 
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetchDataFromFirestore();
-      setProdukData(data);
-    }
-    fetchData();
-  }, []);
+	useEffect(() => {
+		async function fetchData() {
+			const data = await fetchDataFromFirestore();
+			setProdukData(data);
+		}
+		fetchData();
+	}, []);
 
-  useEffect(() => {
-    const sortData = () => {
-      let sortedData = [...produkData];
-      switch (filter) {
-        case "produk":
-          sortedData.sort((a, b) => a.Nama.localeCompare(b.Nama));
-          break;
-        case "UPC":
-          sortedData.sort((a, b) => a.id.localeCompare(b.id));
-          break;
-        case "Harga":
-          sortedData.sort((a, b) => a.Harga - b.Harga);
-          break;
-        default:
-          break;
-      }
-      setProdukData(sortedData);
-    };
+	useEffect(() => {
+		const sortData = () => {
+			let sortedData = [...produkData];
+			switch (filter) {
+				case "produk":
+					sortedData.sort((a, b) => a.Nama.localeCompare(b.Nama));
+					break;
+				case "UPC":
+					sortedData.sort((a, b) => a.id.localeCompare(b.id));
+					break;
+				case "Harga":
+					sortedData.sort((a, b) => a.Harga - b.Harga);
+					break;
+				default:
+					break;
+			}
+			setProdukData(sortedData);
+		};
 
-    sortData();
-  }, [filter, produkData]);
+		sortData();
+	}, [filter, produkData]);
 
-  const indexOfLastItem: number = currentPage * itemsPerPage;
-  const indexOfFirstItem: number = indexOfLastItem - itemsPerPage;
-  const currentItems: ProdukData[] = produkData.slice(indexOfFirstItem, indexOfLastItem);
+	const indexOfLastItem: number = currentPage * itemsPerPage;
+	const indexOfFirstItem: number = indexOfLastItem - itemsPerPage;
+	const currentItems: ProdukData[] = produkData.slice(indexOfFirstItem, indexOfLastItem);
 
-  const paginatePrevious = () => setCurrentPage(currentPage - 1);
-  const paginateNext = () => setCurrentPage(currentPage + 1);
+	const paginatePrevious = () => setCurrentPage(currentPage - 1);
+	const paginateNext = () => setCurrentPage(currentPage + 1);
 
-  return (
-    <main className="min-h-screen flex-col items-center p-24 bg-[#f4f9fb]">
-      <Navbar isDashboardPage={false} isProdukPage={true} isAdminPage={false} />
+	return (
+		<main className="min-h-screen flex-col items-center p-24 bg-[#f4f9fb]">
+			<Navbar isDashboardPage={false} isProdukPage={true} isAdminPage={false} />
 
-      <div className="fixed right-10 top-10">
-        <a href="/produk/tambahProduk" className="bg-[#25B6D0] hover:bg-[#64abba] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md hover:shadow-lg ml-4">
-          Tambah Produk
-        </a>
-      </div>
+			<div className="fixed right-10 top-10">
+				<a
+					href="/produk/tambahProduk"
+					className="bg-[#25B6D0] hover:bg-[#64abba] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md hover:shadow-lg ml-4"
+				>
+					Tambah Produk
+				</a>
+			</div>
 
-      <div className="fixed right-10">
-        <select id="filter" name="filter" value={filter} onChange={(e) => setFilter(e.target.value)} className="font-poppins text-[15px] text-[#243956] w-[135px] 2xl:w-[165px] h-[32px] 2xl:h-[48px]  pl-10 rounded border border-[#D4E3E8]">
-          <option value="produk">Produk</option>
-          <option value="UPC">UPC</option>
-          <option value="Harga">Harga</option>
-        </select>
-      </div>
+			<div className="fixed right-10">
+				<select
+					id="filter"
+					name="filter"
+					value={filter}
+					onChange={(e) => setFilter(e.target.value)}
+					className="font-poppins text-[15px] text-[#243956] w-[135px] 2xl:w-[165px] h-[32px] 2xl:h-[48px]  pl-10 rounded border border-[#D4E3E8]"
+				>
+					<option value="produk">Produk</option>
+					<option value="UPC">UPC</option>
+					<option value="Harga">Harga</option>
+				</select>
+			</div>
 
-      <div className="fixed left-64 ml-10 mt-16">
-        <table>
-          <thead className="bg-[#E4F2F6]">
-            <tr>
-              <th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px] w-[300px] max-w-[300px] 2xl:w-[595px] 2xl:max-w-[595px]">Produk</th>
-              <th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px]">UPC</th>
-              <th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px]">Harga</th>
-              <th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px]">Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentItems.map((produk) => (
-              <tr className="bg-white" key={produk.id}>
-                <td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] w-[300px] max-w-[300px] 2xl:w-[595px] 2xl:max-w-[595px] flex items-center">{produk.Nama}</td>
-                <td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px] text-center">{produk.id}</td>
-                <td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px] text-center">Rp {produk.Harga},00</td>
-                <td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] text-center">
-                  {/* <button
+			<div className="fixed left-64 ml-10 mt-16">
+				<table>
+					<thead className="bg-[#E4F2F6]">
+						<tr>
+							<th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px] w-[300px] max-w-[300px] 2xl:w-[595px] 2xl:max-w-[595px]">Produk</th>
+							<th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px]">UPC</th>
+							<th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px]">Harga</th>
+							<th className="border border-[#D4E3E8] text-black py-[15px] px-[5px] font-poppins text-[16px]">Aksi</th>
+						</tr>
+					</thead>
+					<tbody>
+						{currentItems.map((produk) => (
+							<tr className="bg-white" key={produk.id}>
+								<td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] w-[300px] max-w-[300px] 2xl:w-[595px] 2xl:max-w-[595px] flex items-center">{produk.Nama}</td>
+								<td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px] text-center">{produk.id}</td>
+								<td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] w-[150px] max-w-[150px] 2xl:w-[225px] 2xl:max-w-[225px] text-center">
+									Rp {produk.Harga},00
+								</td>
+								<td className="border-y border-y-[#E0E0E0] text-black py-[15px] px-[30px] font-poppins text-[12px] 2xl:text-[16px] text-center">
+									{/* <button
                     onClick={() => handleEdit(produk.id, produk.Nama, produk.Harga)}
                     className="bg-[#25B6D0] hover:bg-[#64abba] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md hover:shadow-lg mr-2"
                   >
                     Edit
                   </button> */}
-                  <button
-                    onClick={() => handleDelete(produk.id)}
-                    className="bg-[#FF5C5C] hover:bg-[#FF5C5C] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md hover:shadow-lg"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+									<button
+										onClick={() => handleDelete(produk.id)}
+										className="bg-[#FF5C5C] hover:bg-[#FF5C5C] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md hover:shadow-lg"
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 
-      {/* Tambahkan navigasi pagination */}
-      <div className="pagination b-10 flex justify-center">
-        <button className="bg-[#55A3BD] hover:bg-[#5495a2] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md shadow-md mr-4" onClick={paginatePrevious}>
-          Previous
-        </button>
-        <button className="bg-[#55A3BD] hover:bg-[#5495a2] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md shadow-md mr-4" onClick={paginateNext}>
-          Next
-        </button>
-      </div>
-    </main>
-  );
+			{/* Tambahkan navigasi pagination */}
+			<div className="pagination b-10 flex justify-center">
+				<button
+					className="bg-[#55A3BD] hover:bg-[#5495a2] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md shadow-md mr-4"
+					onClick={paginatePrevious}
+				>
+					Previous
+				</button>
+				<button className="bg-[#55A3BD] hover:bg-[#5495a2] py-[9px] 2xl:py-[12px] px-[22px] 2xl:px-[30px] rounded-md font-poppins text-[14px] 2xl:text-[16px] drop-shadow hover:drop-shadow-md shadow-md mr-4" onClick={paginateNext}>
+					Next
+				</button>
+			</div>
+		</main>
+	);
 };
 
 export default Produk;
